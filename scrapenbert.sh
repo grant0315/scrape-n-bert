3a356c7507f2ceab2d87e7792a536a0cf7c98eff
Grant Hopkins <granthopkins0315@gmail.com>
2022-04-06 22:23:17 -0500

    Goodluck future me.

---
+++ scrapenbert.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 -t type_of_snb_run"
+    echo "Types (the types of snb runs):"
+    echo "  Optional -t individual-snb Runs scrape-n-bert from config file, and run bert instance on each domain"
+    echo "  Optional -t only-scrape Runs spider on domains in config file"
+    echo "  Optional -t only-bert Runs bertopic instance on each domain in config file without scraping"
+    echo "  Optional -t combined-bert Combines multiple scraped data files, and runs bertopic on top of the large file"
+    echo
+}
+
+while getopts "t:" opt
+do
+    case "$opt" in
+        t ) TYPE="$OPTARG" ;;
+        h ) helpFunction ;;
+        ? ) helpFunction ;;
+        * ) helpFunction ;;
+    esac
+done
+
+case "$TYPE" in
+    "individual-snb") 
+        echo "Running Scrape-n-bert in type: $TYPE\n"
+        
+        ;;
+
+    "only-scrape")
+        echo "Running Scrape-n-bert in type: $TYPE\n"
+        ;;
+
+    "only-bert")
+        echo "Running Scrape-n-bert in type: $TYPE\n"
+        ;;
+
+    "combined-bert")
+        echo "Running Scrape-n-bert in type: $TYPE\n"
+        ;;
+
+    *)
+        helpFunction ;;
+esac
