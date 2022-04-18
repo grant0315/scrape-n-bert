f77bd700bb32ad274a721ec9262c2b58bf44868d
whopkinsthomas <74738159+whopkinsthomas@users.noreply.github.com>
2022-04-18 14:15:59 -0500

    Added only-keybert to entry.py as well as function

--- src/py/keybert_wrapper/main.py
+++ src/py/keybert_wrapper/main.py
@@ -1,5 +1,4 @@
 from keybert import KeyBERT
-import pickle as p 
 
 class KeybertWrapper:
     def __init__(self, docs):
@@ -7,8 +6,8 @@ class KeybertWrapper:
         self.kw_model = KeyBERT()
 
     def find_keywords(self):
-        keywords = self.kw_model.extract_keywords(self.docs)
+        keywords = self.kw_model.extract_keywords(self.docs, keyphrase_ngram_range=(1,5), top_n=10, stop_words='english')
         return keywords
 
-    def write_keywords_to_disk(self):
+    def write_keywords_to_disk(self, keywords):
         pass
--- src/py/keybert_wrapper/test.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-
-
-filename = "p_rep_docs"
-outfile = open(filename, 'wb')
-pickle.dump(rep_docs, outfile)
-outfile.close()
